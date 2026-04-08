56697dadb8950e0ec946b3fb2a777bac5528215c
Bhavyanshi Karela <bhavyanshikarela500@gmail.com>
2026-04-09 00:09:35 +0530

    Changed the frontend a little

--- client-ui/src/App.js
+++ client-ui/src/App.js
@@ -13,70 +13,73 @@ function App() {
   const [isMockMode, setIsMockMode] = useState(false);
 
   return (
-    <Router>
-      <Routes>
-        {/* Redirect root to chat if logged in, else login */}
-        <Route 
-          path="/" 
-          element={token ? <Navigate to="/chat" replace /> : <Navigate to="/login" replace />} 
-        />
-        
-        <Route 
-          path="/login" 
-          element={
-            token ? <Navigate to="/chat" replace /> :
-            <Login 
-              setToken={setToken} 
-              setUsername={setUsername} 
-              isMockMode={isMockMode} 
-              setIsMockMode={setIsMockMode} 
-            />
-          } 
-        />
-        
-        <Route 
-          path="/register" 
-          element={
-            token ? <Navigate to="/chat" replace /> :
-            <Register 
-              setToken={setToken} 
-              setUsername={setUsername} 
-              isMockMode={isMockMode} 
-              setIsMockMode={setIsMockMode} 
-            />
-          } 
-        />
-        
-        <Route 
-          path="/forgot-password" 
-          element={<ForgotPassword isMockMode={isMockMode} />} 
-        />
-        
-        <Route 
-          path="/reset-password" 
-          element={<ResetPassword isMockMode={isMockMode} />} 
-        />
-        
-        <Route 
-          path="/chat" 
-          element={
-            !token ? <Navigate to="/login" replace /> :
-            <ChatWindow 
-              token={token} 
-              username={username || "TestUser"} 
-              isMockMode={isMockMode} 
-              onLogout={() => { 
-                setToken(null); 
-                setIsMockMode(false); 
-              }} 
-            />
-          } 
-        />
-        
-        {/* Catch all unhandled routes */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="animated-bg"></div>
+      <Router>
+        <Routes>
+          {/* Redirect root to chat if logged in, else login */}
+          <Route 
+            path="/" 
+            element={token ? <Navigate to="/chat" replace /> : <Navigate to="/login" replace />} 
+          />
+          
+          <Route 
+            path="/login" 
+            element={
+              token ? <Navigate to="/chat" replace /> :
+              <Login 
+                setToken={setToken} 
+                setUsername={setUsername} 
+                isMockMode={isMockMode} 
+                setIsMockMode={setIsMockMode} 
+              />
+            } 
+          />
+          
+          <Route 
+            path="/register" 
+            element={
+              token ? <Navigate to="/chat" replace /> :
+              <Register 
+                setToken={setToken} 
+                setUsername={setUsername} 
+                isMockMode={isMockMode} 
+                setIsMockMode={setIsMockMode} 
+              />
+            } 
+          />
+          
+          <Route 
+            path="/forgot-password" 
+            element={<ForgotPassword isMockMode={isMockMode} />} 
+          />
+          
+          <Route 
+            path="/reset-password" 
+            element={<ResetPassword isMockMode={isMockMode} />} 
+          />
+          
+          <Route 
+            path="/chat" 
+            element={
+              !token ? <Navigate to="/login" replace /> :
+              <ChatWindow 
+                token={token} 
+                username={username || "TestUser"} 
+                isMockMode={isMockMode} 
+                onLogout={() => { 
+                  setToken(null); 
+                  setIsMockMode(false); 
+                }} 
+              />
+            } 
+          />
+          
+          {/* Catch all unhandled routes */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
